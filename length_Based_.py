d30f102f6258c07568a160019d85b1c2eb3452e9
farhanrhine <mohammadfarhanalam09@gmail.com>
2025-06-09 21:10:11 +0530

    done

---
+++ length_Based_.py
@@ -0,0 +1,22 @@
+from cgitb import text
+from langchain.text_splitter import CharacterTextSplitter
+text = """Space exploration has led to incredible scientific discoveries.
+From landing on the Moon to exploring Mars, humanity
+continues to push the boundaries of what's possible beyond our
+planet.
+These missions have not only expanded our knowledge of the
+universe but have also contributed to advancements in
+technology here on Earth. Satellite communications, GPS, and
+even certain medical imaging techniques trace their roots back
+to innovations driven by space programs.
+"""
+# splitter object
+splitter = CharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=10,
+    separator=''
+)
+
+result = splitter.split_text(text)
+
+print(result)
--- semantic_meaning_based.py
+++ semantic_meaning_based.py
@@ -1,12 +1,12 @@
 from langchain_experimental.text_splitter import SemanticChunker
-from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_ollama.embeddings import OllamaEmbeddings
 from dotenv import load_dotenv
 
 load_dotenv()
 
 text_splitter = SemanticChunker(
-    OpenAIEmbeddings(), breakpoint_threshold_type="standard_deviation",
-    breakpoint_threshold_amount=3
+    OllamaEmbeddings(model="nomic-embed-text"), breakpoint_threshold_type="standard_deviation",
+    breakpoint_threshold_amount=1
 )
 
 sample = """
